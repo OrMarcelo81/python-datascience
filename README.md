c954ae68247082647f879eebf7037bd7142782dc
Orlando <orlandomarcelo.vazquez@gmail.com>
2023-08-31 15:53:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python para Ciencia de Datos
+
+Este repositorio contiene los archivos que iremos trabajando durante el módulo.
---
+++ ucom-mysql-conn.py
@@ -0,0 +1,41 @@
+import pip
+pip.main(["install", "mysql-connector-python"])
+import mysql.connector, json
+
+# se lee la configuración
+with open("config.json", "r") as f:
+    config = json.load(f)
+    user = config['user']
+    password = config['password']
+    host = config['host']
+    database = config['database']
+
+# Establecer la conexión
+conn = mysql.connector.connect(
+    user = user,
+    password = password,
+    host = host,
+    database = database
+)
+
+# Crear un cursor para ejecutar consultas
+cursor = conn.cursor()
+
+# Ejecutar una consulta
+consulta = "SELECT * FROM PROVEEDORES_DEL_ESTADO;"
+cursor.execute(consulta)
+
+# Obtener los resultados de la consulta
+resultados = cursor.fetchall()
+
+# Recorrer los resultados
+contador = 0
+for fila in resultados:
+    print(fila)
+    contador += 1
+    if contador == 20:
+        break
+
+# Cerrar el cursor y la conexión
+cursor.close()
+conn.close()
